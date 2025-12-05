fe6bdf88a16d9461085ed4326ce711ddf4bc6ed2
Sudo-su-Bash <SudosuBash@outlook.com>
2025-12-05 20:05:07 +0800

    bug fix (2)

--- src/controllers/ecall.c
+++ src/controllers/ecall.c
@@ -45,7 +45,8 @@ void irq_call(PWOLF_CPU_ECALL_CONTROLLER* ctrl) {
 
 void eret(PWOLF_CPU_ECALL_CONTROLLER* ctrl) {
     WOLF_CPU* cpu = get_parent_struct(ctrl, WOLF_CPU, ecall_controller);
-    cpu->spe_regs.bcr |= (GET_DATA_BIT_VALUE(cpu->ecall_regs.mprevmode,KU_STORE_BITS) << (BCR_KERN_MODE_MASK)) | (GET_DATA_BIT_VALUE(cpu->ecall_regs.mprevmode,IRQ_STORE_BITS) << (BCR_IRQ_DISALLOW_MASK_BIT)); //恢复特权级和中断使能
+    cpu->spe_regs.bcr |= (GET_DATA_BIT_VALUE(cpu->ecall_regs.mprevmode,KU_STORE_BITS) << (BCR_KERN_MODE_MASK)); //恢复特权级
+    cpu->spe_regs.bcr &= 0b11111111 ^ (GET_DATA_BIT_VALUE(cpu->ecall_regs.mprevmode,IRQ_STORE_BITS) << (BCR_IRQ_DISALLOW_MASK_BIT)); //恢复中断使能
     //日后会新增4条特权指令设用来设置ecall的mep mprevmode,用来软件保存状态
     cpu->pc = cpu->ecall_regs.mpc + 4; //跳过当前指令
     cpu->ecall_regs.mep = 0;
